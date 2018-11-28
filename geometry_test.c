67d34142c54be75a27d6ce1401a26abb30c9d34b
Desmond Blue Manthy <blue@Desmonds-MacBook-Pro.local>
2018-11-28 16:04:22 -0700

    please

--- geometry_test.c
+++ geometry_test.c
@@ -15,6 +15,24 @@
 
 #include "geometry.h"
 
+START_TEST(test_tri)
+{
+    coord_2d_t a, b, c;
+
+    a.x = b.x = c.x = 0;
+    a.y = b.y = c.y = 0;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0.0);
+
+    a.x = 0;
+    b.x = 0;
+    c.x = 36;
+    a.y = 9;
+    b.y = 12;
+    c.y = 20;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 306);
+}
+END_TEST
+
 /* coord_2d_eq Test */
 START_TEST(test_2d_eq)
 {
@@ -162,10 +180,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* coord_2d_area_triangle = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(coord_2d_area_triangle, coord_2d_area_triangle);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, coord_2d_area_triangle);
 
     /* Return Suite */
     return s;
